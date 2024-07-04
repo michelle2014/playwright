a1a1525056983cf82aac9097377d2723aae27ef8
Michelle <michelle.transbetter@gmail.com>
2024-07-04 14:52:12 +1000

    update visual comparison to partial area

--- tests/visual-comparisons.spec.ts
+++ tests/visual-comparisons.spec.ts
@@ -2,7 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('visual comparisons test', async ({ page }) => {
   await page.goto('https://playwright.dev');
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
+  await expect(page).toHaveScreenshot({
+    maxDiffPixels: 100,
+    // crop the screenshot to a specific area
+    clip: { x: 0, y: 0, width: 500, height: 500 },
+  });
 });
 
 // to compare text or arbitrary binary data
