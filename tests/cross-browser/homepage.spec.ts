a9ba502dc196f77453be0d3a62a231f38c352f3d
Michelle <michelle.transbetter@gmail.com>
2024-07-05 15:24:59 +1000

    add cross browser visual comparison tests

---
+++ tests/cross-browser/homepage.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('cross-browser snapshots', async ({ page, }) => {
+  await page.goto('https://www.browsercat.com');
+  await page.setViewportSize({
+    width: 1280,
+    height: 7344,
+  });
+  await page.locator(':has(> figure)')
+    .evaluate(($el) => $el.remove());
+
+  await expect(page).toHaveScreenshot(`home-page.png`, {
+    fullPage: true,
+    maxDiffPixelRatio: 0.02
+  });
+});
