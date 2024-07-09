e4ec062dcf8dd19986162b4e7f86aef055f75f6a
Michelle <michelle.transbetter@gmail.com>
2024-07-09 15:01:36 +1000

    comment out cross browser tests

--- tests/cross-browser/homepage.spec.ts
+++ tests/cross-browser/homepage.spec.ts
@@ -1,18 +1,20 @@
-import { test, expect } from '@playwright/test';
+// This test is commented out coz the whole page comparison is running too slow in the pipeline
+// Probably not a good idea to compare the whole page for different browsers
+// import { test, expect } from '@playwright/test';
 
-test('cross-browser snapshots', async ({ page, }) => {
-  await page.goto('https://www.browsercat.com');
-  await page.setViewportSize({
-    width: 1280,
-    height: 7344,
-  });
-  await page.locator(':has(> figure)')
-    .evaluate(($el) => $el.remove());
+// test('cross-browser snapshots', async ({ page, }) => {
+//   await page.goto('https://www.browsercat.com');
+//   await page.setViewportSize({
+//     width: 1280,
+//     height: 7344,
+//   });
+//   await page.locator(':has(> figure)')
+//     .evaluate(($el) => $el.remove());
 
-  await expect(page).toHaveScreenshot(`home-page.png`, {
-    fullPage: true,
-    maxDiffPixelRatio: 0.1,
-    // retry snapshot until timeout is reached
-    timeout: 1000 * 60,
-  });
-});
+//   await expect(page).toHaveScreenshot(`home-page.png`, {
+//     fullPage: true,
+//     maxDiffPixelRatio: 0.1,
+//     // retry snapshot until timeout is reached
+//     timeout: 1000 * 60,
+//   });
+// });
